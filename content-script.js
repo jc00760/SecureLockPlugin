e0d96a58bce314bd7737d7d32fa3bacdf45c6644
jc00760 <66928679+jc00760@users.noreply.github.com>
2023-12-12 23:02:41 -0500

    tried many different libraries, going to try declarativeNetRequest for now

---
+++ content-script.js
@@ -0,0 +1,18 @@
+// content-script.js
+// This script is injected into pages that match the specified URL pattern.
+
+// Create a new div element
+// document.documentElement.innerHTML = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//       <title>New Page Title</title>
+//     </head>
+//     <body>
+//       <h1>Hello from My Extension!</h1>
+//       <p>This is the new content.</p>
+//     </body>
+//   </html>
+// `;
+
+console.log("hi")
