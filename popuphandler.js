a6045bdc4c0c1c14073199027418b26e716e5a4b
jc00760 <66928679+jc00760@users.noreply.github.com>
2023-12-13 17:15:41 -0500

    button event handlers + lock() functionality. lock() now retrieves old rules and appends a new rule. pages must be refreshed to see blocked message, though.

--- popuphandler.js
+++ popuphandler.js
@@ -1,27 +1,77 @@
+// HTML button elements
+
+window.onload = function () {
+  const buttonLock = document.getElementById("lock");
+  buttonLock.addEventListener("click", buttonClick);
+};
+
+function buttonClick() {
+  // const url = window.location.href;
+  console.log("lock clicked");
+
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    let url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+    console.log(url);
+    lock(url);
+  });
+}
+
+// // button1 click event listener to apply function
+// buttonLock.addEventListener("click", function() {
+//     // your function code here
+//     // get url and call lock with it
+//     const url = window.location.href;
+//     console.log("lock clicked")
+//     console.log(url);
+//     // lock(url)
+// });
+
+// [
+//   {
+//     id: 1,
+//     priority: 1,
+//     action: { type: "block" },
+//     condition: { urlFilter: "netflix.com", resourceTypes: ["main_frame"] },
+//   },
+// ];
+
 /**
  * Locks the url given by appending a new rule
  * @param {string} url- The URL to be locked
  */
-function lock(url) {
+async function lock(url) {
   // error handling:
   // what if already blocked? shouldn't happen but.
-
-  // extract url
 
   // get existing rules
 
   // create new rule
 
+  
+
   // newRules = oldRules + new rule
+  const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+  console.log(oldRules)
+  console.log(oldRules.length)
+
+  const new_rule = [{
+    id: oldRules.length + 1,
+    priority: 1,
+    action: { type: "block" },
+    condition: { urlFilter: url, resourceTypes: ["main_frame"] },
+  }];
 
   chrome.declarativeNetRequest.updateDynamicRules({
     removeRuleIds: [],
-    addRules: newRules,
+    // addRules: newRules,
+    addRules: new_rule,
   });
+  console.log("done")
 }
 
 /**
- * Unocks the url given by removing it from the ruleset
+ * Unlocks the url given by removing it from the ruleset
  * @param {string} url- The URL to be unlocked
  */
 function unlock(url) {
@@ -37,6 +87,6 @@ function unlock(url) {
 
   chrome.declarativeNetRequest.updateDynamicRules({
     removeRuleIds: [toRemove],
-    addRules:[],
+    addRules: [],
   });
 }
