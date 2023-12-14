b3d4ca34cfcd7824c97c97ceab53760545c95c21
jc00760 <66928679+jc00760@users.noreply.github.com>
2023-12-14 17:41:54 -0500

    basic password functionality

--- popuphandler.js
+++ popuphandler.js
@@ -1,20 +1,5 @@
 // Function to check lock status of current page
 async function checkLockStatus() {
-  // let locked_id = -1;
-  // chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
-  //   let url = tabs[0].url;
-  //   const rules = await chrome.declarativeNetRequest.getDynamicRules();
-  //   // const isLocked = rules.some((rule) => rule.condition.urlFilter === url);
-
-  //   for (const rule of rules) {
-  //     if (rule.condition.urlFilter === url) {
-  //       locked_id = rule.id;
-  //     }
-  //   }
-  //   updateButtonVisibility(locked_id > 0);
-  // });
-  // console.log(locked_id)
-  // return locked_id;
   return new Promise((resolve) => {
     let locked_id = -1;
     chrome.tabs.query(
@@ -30,7 +15,7 @@ async function checkLockStatus() {
           }
         }
 
-        updateButtonVisibility(locked_id > 0);
+        updateFormVisibility(locked_id > 0);
         console.log(locked_id);
         resolve(locked_id);
       }
@@ -38,10 +23,39 @@ async function checkLockStatus() {
   });
 }
 
+// checks if password has been made
+async function getPassword() {
+  return new Promise((resolve) => {
+    let password = undefined;
+
+    chrome.storage.sync.get("password", async (pass) => {
+      password = pass.password;
+      console.log(password);
+      resolve(password);
+    });
+  });
+}
+
+// // checks whether user enters the correct password
+// async function passwordCheck(user_password) {
+//   return new Promise((resolve) => {
+//     let correct = false;
+
+//     chrome.storage.sync.get('password', async (pass) => {
+//       correct = (pass === user_password);
+//       console.log(correct)
+//       resolve(correct);
+//     });
+//   });
+// }
+
 // HTML button elements
 window.onload = function () {
   const buttonLock = document.getElementById("lock");
-  const buttonUnlock = document.getElementById("unlock");
+  // const buttonUnlock = document.getElementById("unlock");
+
+  const creation_form = document.getElementById("passwordCreation");
+  const password_form = document.getElementById("passwordForm");
 
   // check lock status on load
   checkLockStatus().then((locked_id) => {
@@ -50,13 +64,38 @@ window.onload = function () {
     console.log("Updated locked_id:", locked_id);
   });
 
+  // first pw creation
+  creation_form.addEventListener("submit", () => {
+    const user_password = document.getElementById("pw-creation").value;
+    chrome.storage.sync.set({ password: user_password }).then(() => {
+      console.log("password set to", user_password);
+      toggleLock(true);
+    });
+  });
+
+  // checking pw when unlocking
+  password_form.addEventListener("submit", () => {
+    const user_password = document.getElementById("pw").value;
+    // const correct = passwordCheck(user_password);
+
+    // const password = getPassword();
+
+    getPassword().then((password) => {
+      if (password && password === user_password) {
+        toggleLock(false);
+      } else {
+        alert("password incorrect");
+      }
+    });
+  });
+
   buttonLock.addEventListener("click", function () {
     toggleLock(true);
   });
 
-  buttonUnlock.addEventListener("click", function () {
-    toggleLock(false);
-  });
+  // buttonUnlock.addEventListener("click", function () {
+  //   toggleLock(false);
+  // });
 };
 
 function toggleLock(toLock) {
@@ -69,7 +108,7 @@ function toggleLock(toLock) {
     } else {
       unlock();
     }
-    updateButtonVisibility(!toLock);
+    updateFormVisibility(!toLock);
     // updateButtonVisibility(!toLock).then(() => {
     //   chrome.tabs.reload();
     // });
@@ -78,17 +117,59 @@ function toggleLock(toLock) {
 }
 
 // Function to update button visibility based on lock status
-function updateButtonVisibility(isLocked) {
+function updateFormVisibility(isLocked) {
   const buttonLock = document.getElementById("lock");
-  const buttonUnlock = document.getElementById("unlock");
+  // const buttonUnlock = document.getElementById("unlock");
 
-  if (isLocked) {
-    buttonLock.style.display = "none";
-    buttonUnlock.style.display = "block";
-  } else {
-    buttonLock.style.display = "block";
-    buttonUnlock.style.display = "none";
-  }
+  const creation_form = document.getElementById("passwordCreation");
+
+  const password_form = document.getElementById("passwordForm");
+
+  // get password from storage
+  // const password = chrome.storage.sync.get('password')
+  // const password = getPassword();
+  // console.log("password inside updateform visibility", password);
+
+  getPassword().then((password) => {
+    // locked, only password form visible
+    if (isLocked) {
+      password_form.style.display = "block";
+      password_form.style.hidden = "false";
+
+      creation_form.style.display = "none";
+      creation_form.style.hidden = "true";
+
+      buttonLock.style.display = "none";
+    } else if (!isLocked && password) {
+      // unlocked and password exists, lock button visible
+      password_form.style.display = "none";
+      password_form.style.hidden = "true";
+      creation_form.style.display = "none";
+      creation_form.style.hidden = "true";
+
+      buttonLock.style.display = "block";
+    } else {
+      // unlocked and no password, creation form visible
+      password_form.style.display = "none";
+      password_form.style.hidden = "true";
+
+      creation_form.style.display = "block";
+      creation_form.style.hidden = "false";
+      buttonLock.style.display = "none";
+    }
+    console.log(password);
+    // This callback will be executed when the Promise is resolved
+    // The resolved value (locked_id) is passed as an argument to this callback
+    // console.log("Updated locked_id:", locked_id);
+  });
+
+  // if (isLocked) {
+  //   buttonLock.style.display = "none";
+  //   buttonUnlock.style.display = "block";
+  // } else {
+  //   buttonLock.style.display = "block";
+  //   buttonUnlock.style.display = "none";
+  // }
 }
 
 /**
